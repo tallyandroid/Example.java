de3c9ca90df818516a68ad585f9e13b6d2f595fa
Christian Tisby <christiantisby@Christians-Mac-mini.local>
2018-10-05 09:58:35 -0400

    first commit

---
+++ src/Example.java
@@ -0,0 +1,13 @@
+/*
+   This is a simple java program.
+
+   We will call this program Example.java
+
+ */
+
+public class Example {
+    //  A Java program begins with a call to main ().
+    public static void main(String[] args) {
+        System.out.println("Java drives the web");
+    }
+}
